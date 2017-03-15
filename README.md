e6a068ea7fe7171a5034a627c31f8d87ce0ad222
Jbee <Jbee@AL01155703.local>
2017-03-15 18:14:16 +0900

    project_scaffolding

--- README.md
+++
@@ -1,7 +0,0 @@
-# 2017-01-HUDI-MAC-CHAR
-2017년 1학기 휴디 - 웹, 모바일을 위한 저장소
-test
-bbq923 test
-
-android : 변지민
-iOS : 권용재
---
+++ src/main/java/com/mapia/MapiaApplication.java
@@ -0,0 +1,12 @@
+package com.mapia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapiaApplication.class, args);
+	}
+}
