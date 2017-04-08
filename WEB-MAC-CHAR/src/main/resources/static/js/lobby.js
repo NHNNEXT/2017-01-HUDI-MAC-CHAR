7992016ed9edff7cbc373413c6f27a67af11f9a3
JangWoohyeon <wkddngus5@naver.com>
2017-04-08 11:07:19 +0900

    click each room (#100)
    Add click event each room to enter room

--- WEB-MAC-CHAR/src/main/resources/static/js/lobby.js
+++ WEB-MAC-CHAR/src/main/resources/static/js/lobby.js
@@ -3,19 +3,25 @@ window.onload = () => {
 }
 
 function init() {
-	var dimmer = document.querySelector("#dimmer");
+	let dimmer = document.querySelector("#dimmer");
+	let eachRoom = document.querySelector(".eachRoom");
+	
 	document.querySelector("#make_room").addEventListener("click", () => {
 		dimmer.style.display = "block";
 		document.querySelector("#make_room_form").style.display = "block";
 	});
 
-	dimmer.addEventListener("click", () => {
+	dimmer.addEventListener("click", function() {
 		dimmer.style.display = "none";
 		offModal();
 	});
-
+	
+	eachRoom.addEventListener("click", function() {
+		window.location.href = "/room/" + this.id;
+	});
+	
 	function offModal() {
-		var modals = document.querySelectorAll(".my_modal");
+		let modals = document.querySelectorAll(".my_modal");
 		console.log(modals);
 		for (modal of modals) {
 			modal.style.display = "none";
