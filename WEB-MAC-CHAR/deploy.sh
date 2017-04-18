301b3d5feb180074e0324a3b225d3a68eb7e724c
javajigi <javajigi@gmail.com>
2017-04-18 15:20:54 +0900

    revert config

--- WEB-MAC-CHAR/deploy.sh
+++ WEB-MAC-CHAR/deploy.sh
@@ -6,9 +6,7 @@ docker-compose -p mafia up -d
 
 pkill -9 -f mafia-0.0.1.jar
 
-sleep 5
+java -Dspring.profiles.active=dev -jar ./build/libs/mafia-0.0.1.jar > /dev/null 2>&1 &
 
-# java -Dspring.profiles.active=dev -jar ./build/libs/mafia-0.0.1.jar > mafia.log 2>&1 &
-
-nohup java -Dspring.profiles.active=dev -jar ./build/libs/mafia-0.0.1.jar &
+# java -Dspring.profiles.active=dev -jar ./build/libs/mafia-0.0.1.jar &
 
