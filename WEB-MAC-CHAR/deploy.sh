8886a682b5c7477e55048a45c185d188791b8927
javajigi <javajigi@gmail.com>
2017-04-18 15:38:48 +0900

    remove start mafia project command

--- WEB-MAC-CHAR/deploy.sh
+++ WEB-MAC-CHAR/deploy.sh
@@ -6,7 +6,7 @@ docker-compose -p mafia up -d
 
 pkill -9 -f mafia-0.0.1.jar
 
-java -Dspring.profiles.active=dev -jar ./build/libs/mafia-0.0.1.jar > /dev/null 2>&1 &
+# java -Dspring.profiles.active=dev -jar ./build/libs/mafia-0.0.1.jar > /dev/null 2>&1 &
 
 # java -Dspring.profiles.active=dev -jar ./build/libs/mafia-0.0.1.jar &
 
