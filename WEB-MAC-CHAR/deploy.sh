516d2e0a45d271358ad7a3dc65156ba5d18be84e
javajigi <javajigi@gmail.com>
2017-04-18 15:14:09 +0900

    add config nohup

--- WEB-MAC-CHAR/deploy.sh
+++ WEB-MAC-CHAR/deploy.sh
@@ -8,5 +8,5 @@ pkill -9 -f mafia-0.0.1.jar
 
 # java -Dspring.profiles.active=dev -jar ./build/libs/mafia-0.0.1.jar > mafia.log 2>&1 &
 
-java -Dspring.profiles.active=dev -jar ./build/libs/mafia-0.0.1.jar &
+nohup java -Dspring.profiles.active=dev -jar ./build/libs/mafia-0.0.1.jar &
 
