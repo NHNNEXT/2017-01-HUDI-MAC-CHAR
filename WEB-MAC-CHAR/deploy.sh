7c8e7b969d7a81c6e2a109ac81e6d50b4a692894
javajigi <javajigi@gmail.com>
2017-04-18 15:15:47 +0900

    add config sleep

--- WEB-MAC-CHAR/deploy.sh
+++ WEB-MAC-CHAR/deploy.sh
@@ -6,6 +6,8 @@ docker-compose -p mafia up -d
 
 pkill -9 -f mafia-0.0.1.jar
 
+sleep 5
+
 # java -Dspring.profiles.active=dev -jar ./build/libs/mafia-0.0.1.jar > mafia.log 2>&1 &
 
 nohup java -Dspring.profiles.active=dev -jar ./build/libs/mafia-0.0.1.jar &
